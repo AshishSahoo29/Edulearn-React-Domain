d3a1f3afd505a6d4a4a6198c0b5b31bd570c5dd0
ajax899 <ajayawswp.1@gmail.com>
2022-11-10 01:01:45 +0530

    add signup api

--- src/components/Header/Registration.js
+++ src/components/Header/Registration.js
@@ -3,41 +3,104 @@ import React from "react";
 import './Register.css';
 
  
-const Registration = () => {
+export default class Registration extends React.Component  {
+    constructor (){
+        super();
+        this.state={
+            firstName:'',
+            Username:'',
+            Email:'',
+            Password:'',
+            RType:'',
+           
+        }
+        this.firstName=this.firstName.bind(this);
+        this.Username=this.Username.bind(this);
+        this.Email=this.Email.bind(this);
+        this.Password=this.Password.bind(this);
+        this.RType=this.RType.bind(this);
+     }
+
+     firstName(event){
+        this.setState({firstName:event.target.value})
+     }
+
+     Username(event){
+        this.setState({Username:event.target.value})
+     }
+
+     Email(event){
+        this.setState({Email:event.target.value})
+     }
+
+     Password(event){
+        this.setState({Password:event.target.value})
+     }
+     RType(event){
+        this.setState({RType:event.target.value})
+     }
+
+     register(event){
+        fetch('http://localhost:8080/api/users/signup', {
+                  method: 'post',
+                  headers: {
+                    'Accept': '*/*',
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                    name:this.state.firstName,
+                    username:this.state.Username,
+                    email:this.state.Email,
+                    password:this.state.Password,
+                    roles:this.state.RType
+                    
+                  })
+                }).then(Response=>Response.json())
+                    .then((Result)=>{
+                    if(Result.status===1)
+                    alert("Sorry error");
+                    else
+                        alert("Sorry error");
+                  })
+     }
+     
+    render()
+    {
     return (
         <div className="Glasses">
         <div className="Form-glass">
             <form>
                 <div className="New">
-                <label for="fname">First Name</label>
-                <input type="text" id="fname" name="fname" 
+                <label >Full Name</label>
+                <input type="text" onChange={this.firstName} 
                 placeholder="Your name.."></input>
-
-                <label for="lname">Last Name</label>
-                <input type="text" id="lname" name="lname" 
-                placeholder="Your  last name.."></input>
-
-                <label for="gender">Gender</label>
-                    <select id="gender" name="gender">
-                    <option value="Female">Female</option>
-                    <option value="Male">Male</option>
-                    <option value="Other">Other</option>
-                    </select>
-
-                <label for="email">Email Id</label>
-                <input type="email" id="email" name="email" 
+        
+                <label >Username</label>
+                <input type="text" onChange={this.Username}
+                placeholder="Your  Username.."></input>
+        
+      
+                  
+        
+                    <label>Type</label>
+                <input type="text" onChange={this.RType} 
                 placeholder="Your email id.."></input>
-
-                <label for="pwd">Password</label>
-                <input type="Password" id="p_word" name="password" 
+         <label>Email Id</label>
+                <input type="text" onChange={this.Email} 
+                placeholder="Your email id.."></input>
+        
+                <label >Password</label>
+                <input type="Password" onChange={this.Password}
                 placeholder="Enter your password.."></input>
-
-                <input type="submit" value="Submit"></input>             
+        
+        
+        
+                <input type="submit" onClick={this.register}/>             
                 </div>
             </form>
         </div> 
         </div>
      );
 }
- 
-export default Registration;
+     }
+
