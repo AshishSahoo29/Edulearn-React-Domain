a96be7d4b666cf099be5ee64f94928cd66d6f754
AshishSahoo29 <sahooasish750@gmail.com>
2022-11-15 15:07:13 +0530

    about page
    structural layout... need modification in style part

--- src/components/Header/About.js
+++ src/components/Header/About.js
@@ -1,10 +1,93 @@
 import React from "react";
+import './About.css';
+import ashish from '../images/ashish.jpg';
  const About = () => {
     return ( 
-        <div>
+        <div className="content-main">
             <h1>
-                 Know About Us
+                 EduLearn
             </h1>
+            <p>
+            EduLearn is an educational technology  offering a communication, collaboration, and coaching platform for many schools and teachers.
+            <br/>Designed by students of Software Technology Domain
+            <br/>Centurion University 
+            </p>
+
+            <div className="team-mate">
+                <div className="board">
+                    <div className="card1">
+                    <img src="..." class="card-img-top" alt="..."/>
+                        <h5>Ajay Gupta</h5>
+                        <div className="pra">
+                            <p>Software Technology</p>
+                            <p style={{align:'center'}}>
+                                <a className="button" href="/#">Read More</a>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="card1">
+                    <img src="..." class="card-img-top" alt="..."/>
+                        <h5>Md Faizan</h5>
+                        <div className="pra">
+                            <p>Software Technology</p>
+                            <p style={{align: 'center'}}>
+                                <a className="button" href="/#">Read More</a>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="card1">
+                    <img src="" class="card-img-top" alt="..."/>
+                        <h5>Ashish Kumar Sahoo</h5>
+                        <div className="pra">
+                            <p>Software Technology</p>
+                            <p style={{align: 'center'}}>
+                                <a className="button" href="/#">Read More</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="board">
+                    <div className="card1">
+                    <img src="..." class="card-img-top" alt="..."/>
+                        <h5>Bhagabat SS Mohapatra</h5>
+                        <div className="pra">
+                            <p>Software Technology</p>
+                            <p style={{align:'center'}}>
+                                <a className="button" href="/#">Read More</a>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="card1">
+                    <img src="..." class="card-img-top" alt="..."/>
+                        <h5>Nishant Biswal</h5>
+                        <div className="pra">
+                            <p>Software Technology</p>
+                            <p style={{align: 'center'}}>
+                                <a className="button" href="/#">Read More</a>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="card1">
+                    <img src="..." class="card-img-top" alt="..."/>
+                        <h5>Rohit Sahoo</h5>
+                        <div className="pra">
+                            <p>Software Technology</p>
+                            <p style={{align: 'center'}}>
+                                <a className="button" href="/#">Read More</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+
         </div>
      );
  }
