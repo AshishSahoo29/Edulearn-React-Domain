6ba4eb1875bf552b39a2f69402a732c547371aac
ajax899 <ajayawswp.1@gmail.com>
2022-11-16 00:23:59 +0530

    api done

--- src/components/Header/Registration.js
+++ src/components/Header/Registration.js
@@ -1,10 +1,51 @@
-import React, { Component } from "react";
+import React, {useRef} from "react";
 import { Button, Form, Input,InputGroup } from 'reactstrap';
 import './Register.css';
 
  
-export default class Registration extends Component  {
-    constructor (props){
+
+const Registration = () => {
+   const Name = useRef(null);
+   const Email = useRef(null);
+   const Username = useRef(null);
+   const Password = useRef(null);
+   const RType = useRef(null);
+/*
+  function handleClick() {
+   console.log(Name.current.value);
+   console.log(Email.current.value);
+   console.log(Username.current.value);
+   console.log(Password.current.value);
+   console.log(RType.current.value);
+  
+  }*/
+  function register(){
+   alert(Email.current.value);
+     fetch('http://localhost:8080/api/users/signup', {
+               method: 'post',
+               headers: {
+                 'Accept': '*/*',
+                 'Content-Type': 'application/json'
+               },
+               body: JSON.stringify({
+                  name:Name.current.value,
+                  username:Username.current.value,
+                 email:Email.current.value,
+                 password:Password.current.value,
+                 roles:RType.current.value
+                 
+               })
+             }).then(Response=>Response.json())
+                 .then((Result)=>{
+                 if(Result.status===1){
+                 alert("Successfully");}
+                 else{
+                     alert("Sorry error");
+               }})
+             //  console.log(body)
+
+  }
+   /*constructor (props){
         super(props);
         this.state={
             firstName:'',
@@ -13,13 +54,19 @@ export default class Registration extends Component  {
             Password:'',
             RType:'',
            
-        };/*
+        };
+      */ 
+        
+       //};
+        
+        
+        /*
         this.firstName=this.firstName.bind(this);
         this.Username=this.Username.bind(this);
         this.Email=this.Email.bind(this);
         this.Password=this.Password.bind(this);
         this.RType=this.RType.bind(this);*/
-     }
+     
 /*
      firstName(event){
         this.setState({firstName:event.value})
@@ -40,76 +87,52 @@ export default class Registration extends Component  {
         this.setState({RType:event.target.value})
      }
      */
-     register(){
-      alert(this.state.firstName);
-        fetch('http://localhost:8080/api/users/signup', {
-                  method: 'post',
-                  headers: {
-                    'Accept': '*/*',
-                    'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify({
-                     firstName:this.state.firstName,
-                     Username:this.state.Username,
-                    Email:this.state.Email,
-                    Password:this.state.Password,
-                    RTpye:this.state.RType
-                    
-                  })
-                }).then(Response=>Response.json())
-                    .then((Result)=>{
-                    if(Result.status===1)
-                    alert("Sorry error");
-                    else
-                        alert("Sorry error");
-                  })
-     }
      
-    render()
-    {
+   
+
     return (
         <div className="Glasses">
         <div className="Form-glass">
             <Form>
                 <InputGroup className="mb-3">
 
-                      <Input type="text" onChangeText={this.firstName} />
+                      <input type="text" ref={Name}  id="message" name="Name"/>
 
                     </InputGroup>
         
-                 <InputGroup className="mb-3">
+                    <InputGroup className="mb-3">
 
-                      <Input type="text"  onChange={this.Username} placeholder="Enter Employee Name" />
+                     <input type="text" ref={Username} placeholder="Enter Username" />
 
-                    </InputGroup>
-
-
-                 <InputGroup className="mb-3">
-
-                      <Input type="text"  onChange={this.RType} placeholder="Enter Employee Name" />
-
-                    </InputGroup>
+                     </InputGroup>
 
 
-              <InputGroup className="mb-3">
+                     <InputGroup className="mb-3">
 
-                      <Input type="text"  onChange={this.Email} placeholder="Enter Employee Name" />
+                     <input type="text"  ref={RType} placeholder="Enter Type" />
 
-                    </InputGroup>
+                     </InputGroup>
 
 
-                   <InputGroup className="mb-3">
+                     <InputGroup className="mb-3">
 
-                      <Input type="text"  onChange={this.Password} placeholder="Enter Employee Name" />
+                     <input type="text"  ref={Email} placeholder="Enter Email" />
 
-                    </InputGroup>
+                     </InputGroup>
 
-                    <Button  onClick={this.register}  color="success" block>Create Account</Button>             
+
+                     <InputGroup className="mb-3">
+
+                     <input type="text"  ref={Password} placeholder="Enter Password" />
+
+                     </InputGroup>
+                           
+                    <Button  onClick={register}  color="success" block>Create Account</Button>             
                 
             </Form>
         </div> 
         </div>
      );
-}
-     }
+    }
 
+export default Registration;
